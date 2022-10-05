2790b964b712d55f35dd77d1d519412cd3563079
Syed Arshad <syedarshad80077@gmail.com>
2022-10-05 19:45:35 +0530

    improved visuals

--- menu.py
+++ menu.py
@@ -1,3 +1,3 @@
-f_category = ["starters",'Main Course','deserts','Chat','snacks','western','juice']
+type_f = ["starters",'Main Course','deserts','Chat','snacks','western','juice']
 commands = ['order','start','help']
 food = ['lemon juice','dosa','idly','vaada','pongal','poori']
--- ordering_reimagined.py
+++ ordering_reimagined.py
@@ -1,39 +1,57 @@
-import f_category
 import telebot
 import menu
 import os
-api_key = secret.TOKEN
+#api_key = os.TOKEN
+
+
+#global 
+
 bot = telebot.TeleBot(TOKEN)
 count = False
 bill = ''
 bill_no = 1
+
+#handlers
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, "Hello World")
+
 @bot.message_handler(commands=['order'])
 def show_category(message):
 	cat_list = ""
 	i = 1
-	for item in menu.category:
+	for item in menu.type_f:
 		cat_list +=f"{i}. {item}\n"
 		i+=1
 	bot.reply_to(message, cat_list)
 	bot.send_message(message.chat.id,"Pick a category")
+
+@bot.message_handler(commands=['available'])
+def show_avail(message):
+	avail_f = ''
+	for food in menu.food:
+		avail_f += food + "\n"
+	bot.reply_to(message,avail_f)
+
 @bot.message_handler(func = lambda m:True)
 def check_menu(message):
+	item = message.text.split(' ',1)
 	global count,bill,bill_no
-	food = str(message.text)
-	food = food.lower()
-	if(str(message.text) in menu.food):
-		if(count != True):
-			count = True
-			bill = f"Bill no :{bill_no} Items \n"
-			bill += message.text + "\n"
-			bot.reply_to(message,bill)
-		else:
-			bill += message.text + '\n'
-			bot.reply_to(message,bill)
-	if(food == "end"):
+	if len(item) == 2 and int(item[0]):
+		qty = int(item[0])
+		food = str(item[1])
+		food = food.lower()
+		if(food in menu.food):
+			if(count != True):
+				count = True
+				bill = f"Bill no :{bill_no}\n--------------------\nQty  Items\n--------------------\n"
+				bill += f"{qty}   X  {food}" + "\n"
+				bot.reply_to(message,bill)
+			else:
+				bill += f"{qty}   X  {food}" + "\n"
+				bot.reply_to(message,bill)
+	if(len(item) == 1 and item[0].lower() == "end"):
 		bot.reply_to(message,"final bill")
 		bot.reply_to(message,bill)
 		count = False
